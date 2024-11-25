2960ccbf37b91037ad8775d2746d5a6167d1d62a
Laurane Chevray <lauranechevray@MacBook-Pro-de-Laurane.local>
2024-11-25 13:17:21 +0100

    bon nom de fichier

--- src/Security/ApiTokenAuthentificator.php
+++ src/Security/ApiTokenAuthentificator.php
@@ -15,7 +15,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 
-class ApiTokenAuthentificatorAuthenticator extends AbstractAuthenticator
+class ApiTokenAuthentificator extends AbstractAuthenticator
 {
     public function __construct(private UserRepository $repository)
     {
